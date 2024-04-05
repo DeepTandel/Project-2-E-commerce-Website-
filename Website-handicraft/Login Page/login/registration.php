beb1cd8d145ef1fe143d13ddddfbf3bba6db64a6
DeepTandel <deeptandel1405@gmail.com>
2024-04-05 12:24:26 +0530

    Project

---
+++ Website-handicraft/Login Page/login/registration.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="phpstyle.css">
+</head>
+<body>
+    <div class="container">
+        <form action="registration.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control"name="repeat_password" placeholder="Repeat Password">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Registration" name="submit">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
